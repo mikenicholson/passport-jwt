fac400d6e4fcec77063dbf961bca6fde0a9ab9e6
Outternet <110238031+Outternet@users.noreply.github.com>
2022-09-10 22:42:18 +0200

    Make fallback work with even older typescript

--- fallback.d.ts
+++ fallback.d.ts
@@ -1,19 +1,31 @@
+import * as index from "./dist/types/index";
+import * as auto from "./dist/types/auto/index";
+import * as jose from "./dist/types/platforms/jose";
+import * as jsonwebtoken from "./dist/types/platforms/jsonwebtoken";
+import * as nestjsjwt from "./dist/types/platforms/nestjsjwt";
+
 declare module "passport-jwt" {
-    export * from "./dist/types/index";
+    export type JwtResult<T> = index.JwtResult<T>
+    export class Strategy<T> extends index.Strategy<T> {}
+    export abstract class JwtDriver<A, B, C> extends index.JwtDriver<A, B, C> {}
+    export class ExtractJwt extends index.ExtractJwt {}
+    export type JwtExtractorType = index.JwtExtractorType;
+    export const FailureMessages: typeof index.FailureMessages;
+    export type JwtStrategyOptions = index.JwtStrategyOptions;
 }
 
 declare module "passport-jwt/auto" {
-    export * from "./dist/types/auto/index";
+    export class Strategy<T> extends auto.Strategy<T> {}
 }
 
 declare module "passport-jwt/platform-jose" {
-    export * from "./dist/types/platforms/jose";
+    export class JoseDriver extends jose.JoseDriver {}
 }
 
 declare module "passport-jwt/platform-jsonwebtoken" {
-    export * from "./dist/types/platforms/jsonwebtoken";
+    export class JsonWebTokenDriver extends jsonwebtoken.JsonWebTokenDriver {}
 }
 
 declare module "passport-jwt/platform-nestjsjwt" {
-    export * from "./dist/types/platforms/nestjsjwt";
+    export class NestjsJwtDriver extends nestjsjwt.NestJsJwtDriver {}
 }
