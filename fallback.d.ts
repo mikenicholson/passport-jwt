80f1912f406a6254ae0d976232fdc98d236cc2da
Outternet <110238031+Outternet@users.noreply.github.com>
2022-09-11 03:54:04 +0200

    fix fallback for intellij

---
+++ fallback.d.ts
@@ -0,0 +1,5 @@
+declare module "passport-jwt" {}
+declare module "passport-jwt/auto" {}
+declare module "passport-jwt/platform-jose" {}
+declare module "passport-jwt/platform-nestjsjwt" {}
+declare module "passport-jwt/platform-jsonwebtoken" {}
