681aa1a211d9b1d1bfcbf1b323dd54c75549a5c1
Mike Nicholson <themikenicholson@gmail.com>
2022-09-12 21:56:42 -0400

    Remove dist directory

--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { JwtStrategy as Strategy, JwtStrategyOptions, VerifyCallback, VerifyCallbackWithReq, SecretOrKeyProvider } from "./jwt_strategy";
-import { ExtractJwt, JwtExtractor as JwtExtractorType } from "./extract_jwt";
-import { JwtDriver, JwtProvidedDriver, JwtResult } from "./platforms/base";
-import { FailureMessages, ErrorMessages } from "./error_messages";
-export { Strategy, ExtractJwt, JwtStrategyOptions, JwtExtractorType, JwtDriver, JwtResult, FailureMessages, ErrorMessages, VerifyCallback, VerifyCallbackWithReq, SecretOrKeyProvider, JwtProvidedDriver };
