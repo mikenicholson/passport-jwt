77c55c0d595b5f8c5d667da834afdd74920cd252
Outternet <110238031+Outternet@users.noreply.github.com>
2022-09-14 17:26:29 +0200

    Update fallback.cjs

--- fallback.cjs
+++ fallback.cjs
@@ -1,4 +1,4 @@
-process.emitWarning("Your node version does not support submodules (which are supported in Node >=12), therefore the fallback is used to load the library as if it had been imported from 'passport-jwt/auto'. You should update your node version, this may work but is **not supported**.");
+process.emitWarning("Your node version does not support submodules (which are supported in Node >= 12), therefore the fallback is used to load the library as if it had been imported from 'passport-jwt/auto'. You should update your node version, this may work but is **not supported**.");
 module.exports = {
     Strategy: require("./dist/cjs/auto_load.cjs").Strategy,
     ExtractJwt: require("./dist/cjs/extract_jwt.cjs").ExtractJwt
