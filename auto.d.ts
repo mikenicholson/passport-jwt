93564b5ae9e7d77428dd07f210fcb39696009746
Outternet <110238031+Outternet@users.noreply.github.com>
2022-09-14 13:55:39 +0200

    Update auto.d.ts

--- auto.d.ts
+++ auto.d.ts
@@ -3,5 +3,5 @@ import * as strategy from "./dist/types/jwt_strategy";
 
 declare module "passport-jwt/auto" {
     export class Strategy<Payload = string, Request extends boolean = false, Verify extends strategy.BasicVerifyCallback<Payload, Request> = strategy.BasicVerifyCallback<Payload, Request>> extends auto.Strategy<Payload, Request, Verify> {}
-    export type JwtAutoStrategyOptions<Request extends boolean = false> = auto.JwtAutoStrategyOptions<Request>;
+    export type JwtAutoStrategyOptions<Request extends boolean = boolean> = auto.JwtAutoStrategyOptions<Request>;
 }
